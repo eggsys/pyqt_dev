77343e7c5eb8c50c4c2f934d069235fc41729a9e
eggsys <46551235+eggsys@users.noreply.github.com>
2021-08-02 16:58:57 +0700

    add 2nd screen

--- project/packages/controller.py
+++ project/packages/controller.py
@@ -1,8 +1,11 @@
 from PyQt5 import QtCore, QtGui, QtWidgets 
 from PyQt5.QtCore import (QCoreApplication, QObject, QRunnable, QThread, QThreadPool, pyqtSignal)
 
-from packages.mainwindow import Mainwindow
+import time
 from packages.ui_welcome import App_WelcomeScreen
+from packages.ui_welcome2 import App_WelcomeScreen2
+
+
 from threading import Thread
 import threading
 
@@ -10,17 +13,36 @@ import threading
 class Controller:
     def __init__(self):
         self.WelcomeScreen = App_WelcomeScreen()
-        print("run !")
-        #Mainwindow()
-        #self.WelcomeScreen.show()
-        self.wtf()
+        self.Screen2 = App_WelcomeScreen2()
+          
+        print("INIT")
+        self.show_Welcome()
         
 
     def show_Welcome(self):
         print("Show welcome")
+        self.WelcomeScreen.show()
+        self.WelcomeScreen.lbl_btn_start.mousePressEvent = self.set_nothing
+        self.WelcomeScreen.lbl_btn_start2.mousePressEvent = self.set_nothing
+        self.WelcomeScreen.btn_register.mousePressEvent = self.set_nothing
+
+    def set_nothing(self, event):
+        print("ping !")
+        self.show_2()
+        
+
         
         print("END!")
+
+    def set_backTo1(self, event):
+        self.WelcomeScreen.show()
+        self.Screen2.close()
 
     def wtf(self):
         self.WelcomeScreen.show()
         print("WTF is here ")
+        time.sleep(5)
+
+    def show_2(self):        
+        self.Screen2.show()
+        self.Screen2.lbl_btn_start.mousePressEvent = self.set_backTo1
--- project/packages/ui_welcome.py
+++ project/packages/ui_welcome.py
@@ -5,6 +5,7 @@ from PyQt5.QtGui import QMovie
 
 
 
+
 class Ui_WelcomeScreen(object):
 
     def setupUi(self, Form):
@@ -13,8 +14,9 @@ class Ui_WelcomeScreen(object):
         Form.resize(1024, 600)
 
         self.lbl_bg_image_id00 = QtWidgets.QLabel(Form)
-        self.lbl_bg_image_id00.setGeometry(QtCore.QRect(0, 0, 1024, 600))             
-        self.gif_welcome_id00 = QMovie('project/images/background/racha_screen.gif')
+        self.lbl_bg_image_id00.setGeometry(QtCore.QRect(0, 0, 1024, 600))    
+
+        self.gif_welcome_id00 = QMovie('project/images/background/k-man-screen.gif')
 
         
 
@@ -27,6 +29,35 @@ class Ui_WelcomeScreen(object):
         self.lbl_bg_image_id00.setScaledContents(True)
         self.lbl_bg_image_id00.setObjectName("lbl_bg_image_id00")
 
+
+        #self.movie.stop()
+        #self.movie.setFileName(self.gifFile)
+        #self.movie.start()
+       
+
+
+
+        self.lbl_btn_start = QtWidgets.QLabel(Form)
+        self.lbl_btn_start.setGeometry(QtCore.QRect(560, 480, 341, 91))
+        self.lbl_btn_start.setText("")
+        self.lbl_btn_start.setPixmap(QtGui.QPixmap("project/images/button/full_check_in-01.png"))
+        self.lbl_btn_start.setScaledContents(True)
+        self.lbl_btn_start.setObjectName("lbl_btn_start")
+
+        self.lbl_btn_start2 = QtWidgets.QLabel(Form)
+        self.lbl_btn_start2.setGeometry(QtCore.QRect(120, 480, 341, 91))
+        self.lbl_btn_start2.setText("")
+        self.lbl_btn_start2.setPixmap(QtGui.QPixmap("project/images/button/full_check_in-01.png"))
+        self.lbl_btn_start2.setScaledContents(True)
+        self.lbl_btn_start2.setObjectName("lbl_btn_start2")
+
+                
+        self.btn_register = QtWidgets.QLabel(Form)
+        self.btn_register.setGeometry(QtCore.QRect(70, 40, 111, 101))
+        self.btn_register.setText("")
+        self.btn_register.setPixmap(QtGui.QPixmap("project/images/button/full_check_in-01.png"))
+        self.btn_register.setScaledContents(True)
+        self.btn_register.setObjectName("btn_back")
 
 
 
@@ -52,4 +83,23 @@ class App_WelcomeScreen(QtWidgets.QWidget, Ui_WelcomeScreen):
         QtWidgets.QWidget.__init__(self)
         self.setWindowFlags(QtCore.Qt.Window | QtCore.Qt.CustomizeWindowHint)
         self.setupUi(self)
+
+        self.lbl_btn_start.mousePressEvent = self.pushbutton_btnstart_handler
+        self.lbl_btn_start2.mousePressEvent = self.pushbutton_btnstart2_handler
+        self.btn_register.mousePressEvent = self.pushbutton_btnregister_handler
+
+
+    def pushbutton_btnstart_handler(self):
+        print(" button press Full FLOW!")
+        self.switch_btnstart_window.emit()
+
+    def pushbutton_btnstart2_handler(self):
+        print(" button press Short FLOW !")
+        self.switch_btnstart2_window.emit()
+
+
+    def pushbutton_btnregister_handler(self):
+        print(" button press Register !")
+        self.switch_btnregister_window.emit()
+
 
